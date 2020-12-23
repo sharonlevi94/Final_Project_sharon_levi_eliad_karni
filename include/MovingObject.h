bc5b2ec954898909353893fa9c2b1d2ea8469f45
sharonlevi94 <sharonlevy96@gmail.com>
2020-12-23 20:04:57 +0200

    10

--- include/MovingObject.h
+++ include/MovingObject.h
@@ -3,16 +3,16 @@
 class MovingObject : public GameObject
 {
 public:
-<<<<<<< HEAD
+
 	MovingObject();
 	
-=======
+
 	MovingObject(
 		const sf::Vector2f location = sf::Vector2f(0, 0),
 		const sf::Vector2f size = sf::Vector2f(0, 0),
 		int state = 0,
 		const sf::Sprite& (*)(char) = nullptr);
->>>>>>> df119aebcdfdccd602898c8cf82565d0722f11d4
+
 	virtual void fall();
 
 private:
