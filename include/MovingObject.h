27e2b131093f412a72378d0d8d9b33fbf9f155ea
sharonlevi94 <sharonlevy96@gmail.com>
2020-12-23 20:02:53 +0200

    11

--- include/MovingObject.h
+++ include/MovingObject.h
@@ -4,10 +4,7 @@ class MovingObject : public GameObject
 {
 public:
 	MovingObject();
-	virtual void draw() const;
-	virtual char identify()const;
-	virtual void reset(const sf::Vector2f&);
-	virtual void playTurn(char (*)(const sf::Vector2f&));
+	
 	virtual void fall();
 
 private:
