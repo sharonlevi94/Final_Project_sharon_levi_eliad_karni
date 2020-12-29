37196d09834792adac7c5aef7b9dc55a431e36e3
sharonlevi94 <sharonlevy96@gmail.com>
2020-12-29 23:11:21 +0200

    update DataReader

--- src/DataReader.cpp
+++ src/DataReader.cpp
@@ -1,6 +1,13 @@
 #include "DataReader.h"
 #include "Macros.h"
 #include "Utilities.h"
+#include <vector>
+#include "Player.h"
+#include "Enemy.h"
+#include "Coin.h"
+#include "Ladder.h"
+#include "Wall.h"
+#include "Rod.h"
 using std::endl;
 //========================================================================
 DataReader::DataReader(){
@@ -17,10 +24,62 @@ DataReader::~DataReader() {
 void DataReader::receiveLevelParameters(){
 	m_boardReader >> m_fileSize.x >> m_fileSize.y;
 	m_boardReader >> m_timeLevel;
-
+	char eat_space;
+	m_boardReader >> eat_space;
 }
 //========================================================================
+/*
+  This function return true if a new level is exist in the inputed file
+  or false if all the levels is over.
+*/
 bool DataReader::isThereNextLevel() {
-	return (this->m_boardReader.peek() == '\n');
+	return (!this->m_boardReader.eof());
 }
 //========================================================================
+vector<vector<GameObject*>> DataReader::readNextLevel() {
+	vector<vector<GameObject*>> newLevel = {};
+	if (this->isThereNextLevel()) {
+		char input;
+		this->receiveLevelParameters();
+		for (int i = 0; i < m_fileSize.x; i++) {
+			std::vector<GameObject*> row = {};
+			for (int j = 0; j < m_fileSize.y; j++) {
+				m_boardReader >> input;
+				switch (input)
+				{
+				case PLAYER: {
+					row.push_back(new Player);
+					break;
+				}
+				case ENEMY: {
+					row.push_back(new Enemy);
+					break;
+				}
+				case COIN: {
+					row.push_back(new Coin);
+					break;
+				}
+				case WALL: {
+					row.push_back(new Wall);
+					break;
+				}
+				case LADDER: {
+					row.push_back(new Ladder);
+					break;
+				}
+				case ROD: {
+					row.push_back(new Rod);
+					break;
+				}
+				default: {
+					row.push_back(NULL); // inputed ' '
+					break;
+				}
+				}
+			}
+			newLevel.push_back(row);
+			m_boardReader >> input; //eat \n
+		}
+	}
+	return newLevel;
+}
