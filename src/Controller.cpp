18581c5b2b7f0018ba04c1dec27123f675635d1f
Eliad Karni <EliadK995@Gmail.com>
2021-01-04 23:06:46 +0200

    Controller -> Menu

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,12 +1,21 @@
 //============================= include section ==============================
 #include "Controller.h"
+#include "Menu.h"
+#include <SFML/Graphics.hpp>
 //============================= public section ===============================
 //==================== Constructors & distructors section ====================
-Controller::Controller() :m_board(Board()), m_effects(EffectsHolder()),
-m_gameobjects({}), m_gameState(GameState()), m_menu(Menu()),
-m_player(nullptr),
-m_window(sf::RenderWindow(sf::VideoMode::getDesktopMode(), 
-	"Rod Runner", sf::Style::Fullscreen)) {}
+Controller::Controller() :
+	m_window(sf::RenderWindow(sf::VideoMode::getDesktopMode(), "Rod Runner", 
+		sf::Style::Fullscreen)),
+	/*m_board(Board()),
+	m_gameobjects({}), m_gameState(GameState()),
+	m_player(nullptr),*/
+	m_effects(EffectsHolder()),
+	m_menu() {
+	// because the 
+	this->m_menu = Menu(Menu(this->m_effects, 
+		(sf::Vector2f)this->m_window.getSize(), sf::Vector2f(0, 0)));
+}
 
 //============================== gets section ================================
 //============================ methods section ===============================
@@ -15,16 +24,51 @@ void Controller::run() {
 	while (true) {
 		this->m_window.clear();
 		char choose = this->runMenu();
-		if (choose == 'q')
+		if (!this->m_window.isOpen() || choose == 'q')
 			break;
 		if (choose == 's')
 			this->runGame();
 	}
 }
 //============================================================================
-void Controller::runMenu() {
+char Controller::runMenu() {
+	while (this->m_window.isOpen())
+	{
+		this->m_window.clear();
+		this->m_menu.draw(this->m_window);
+		this->m_window.display();
+		sf::Event event{};
+		while (this->m_window.waitEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				this->m_window.close();
+				break;
+			case sf::Event::MouseButtonReleased:
+				switch (this->m_menu.handleClick(sf::Vector2f(
+					(float)event.mouseButton.x, 
+					(float)event.mouseButton.y))) {
+				case 's':
+					return 's';
+				case 'q':
+					return 'q';
+					break;
+				}
+				break;
+			default:
+				if (this->m_menu.pointingHundle((sf::Vector2f)
+					sf::Mouse::getPosition(this->m_window))) {
+					this->m_menu.draw(this->m_window);
+					this->m_window.display();
+				}
+				break;
+			}
 
+		}
+	}
+	return 'q';
 }
+//============================================================================
+void Controller::runGame() {}
 //============================ private section ===============================
 //============================== gets section ================================
 //============================ methods section ===============================
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,8 @@
 //============================= include section ==============================
-#include "Menu.h"
-//#include "Board.h"
-#include "EffectsHolder.h"
-#include "SFML/Graphics.hpp"
-#include <Windows.h>
-#include <stdlib.h>
+#include "Controller.h"
 #include <iostream>
 //============================== main section ================================
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode::getDesktopMode(),"Lode Runner", sf::Style::Fullscreen);
-	EffectsHolder effects;
-	//GameState gamestate;
-    Menu menu(effects, (sf::Vector2f)window.getSize(), sf::Vector2f(0,0));
-	//Board board(window.getSize());
-	
-	while (window.isOpen())
-	{
-		window.clear();
-		menu.draw(window);
-		//board.draw(window);     
-		window.display();
-		sf::Event event{};
-		while (window.waitEvent(event)) {
-			switch (event.type) {
-			case sf::Event::Closed:
-				window.close();
-				break;
-			case sf::Event::MouseButtonReleased:
-				switch (menu.handleClick(sf::Vector2f(event.mouseButton.x, event.mouseButton.y))) {
-				case 's':
-					std::cout << "game started!\n";
-					window.close();
-					break;
-				case 'q':
-					std::cout << "quited game!\n";
-					window.close();
-					break;
-				}
-				break;
-			default:
-				if (menu.pointingHundle((sf::Vector2f)sf::Mouse::getPosition(window))) {
-					menu.draw(window);
-					window.display();
-				}
-				break;
-			}
-			
-		}
-	}
-	//gamestate.test(gamestate.died);
-	//gamestate.draw(window);
+int main(){
+	Controller().run();
 	return EXIT_SUCCESS;
 }
