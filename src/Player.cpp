ff05e6322a5e4cd020f74f9e632647d8e8eb6b79
sharonlevi94 <sharonlevy96@gmail.com>
2021-01-06 12:35:06 +0200

    3333

--- src/Player.cpp
+++ src/Player.cpp
@@ -11,13 +11,16 @@
 void Player::draw(sf::RenderWindow& window,const sf::Texture& texture) const {
 	sf::Sprite player;
 	player.setTexture(texture);
+
 	player.setPosition(calcScreenLocation(
 		(sf::Vector2f)window.getSize(),
 		sf::Vector2f(20, 20),
 		this->getLocation()));
+
 	player.setScale(calcScale((sf::Vector2f)window.getSize(),
 		sf::Vector2f(20, 20),
 		(sf::Vector2f)player.getTexture()->getSize()));
+
 	window.draw(player);
 }
 //============================ private section ===============================
