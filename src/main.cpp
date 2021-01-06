4d9cb19eda9fc558a3b3d1385cf11904cb9f4633
sharonlevi94 <sharonlevy96@gmail.com>
2021-01-06 08:11:08 +0200

    menu & board rumming via controller and work

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,12 @@
 //#include "Menu.h"
 //#include "Board.h"
 #include "Controller.h"
-//#include "EffectsHolder.h"
-//#include "SFML/Graphics.hpp"
-//#include <Windows.h>
 #include <stdlib.h>
-//#include <iostream>
 //============================== main section ================================
 int main()
 {
 	Controller controller;
 	controller.run();
-/*	sf::RenderWindow window(sf::VideoMode::getDesktopMode(),"Lode Runner", sf::Style::Fullscreen);
-	Board board(window.getSize());
-	while (window.isOpen())
-	{
-		window.clear();
-		board.draw(window);
-		window.display();
-		sf::Event event;
-		while (window.waitEvent(event)) {
 
-		}
-	}*/
 	return EXIT_SUCCESS;
 }
